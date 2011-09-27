f0c2eaa78755e1d07f649a742063162ff5ef0688
Meno Abels <meno.abels@adviser.com>
2011-09-28 01:26:24 +0200

    return a more complete object on top10

--- informer/src/main/java/com/adviser/informer/Router.java
+++ informer/src/main/java/com/adviser/informer/Router.java
@@ -7,7 +7,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -23,117 +22,113 @@ import com.adviser.informer.model.Streamies;
 import com.adviser.informer.model.streamie.Streamie;
 
 public class Router extends RouteBuilder {
-  public class ListenAddress {
-    private String addr = "127.0.0.1";
-    private String port = "2911";
+	public class ListenAddress {
+		private String addr = "127.0.0.1";
+		private String port = "2911";
 
-    public ListenAddress() {
-    }
+		public ListenAddress() {
+		}
 
-    public ListenAddress(String port, String addr) {
-      this.port = port;
-      if (addr != null)
-        this.addr = addr;
-    }
+		public ListenAddress(String port, String addr) {
+			this.port = port;
+			if (addr != null)
+				this.addr = addr;
+		}
 
-    public String toString() {
-      return addr + ":" + port;
-    }
-  }
+		public String toString() {
+			return addr + ":" + port;
+		}
+	}
 
-  private ListenAddress listenaddress = null;
+	private ListenAddress listenaddress = null;
 
-  private Streamies streamies = null;
+	private Streamies streamies = null;
 
-  public Router(String[] args, Streamies _streamies) {
-    streamies = _streamies;
-    if (args.length == 1) {
-      listenaddress = new ListenAddress(args[0], null);
-    } else if (args.length >= 2) {
-      listenaddress = new ListenAddress(args[0], args[1]);
-    } else {
-      listenaddress = new ListenAddress();
-    }
-    init();
-  }
+	public Router(String[] args, Streamies _streamies) {
+		streamies = _streamies;
+		if (args.length == 1) {
+			listenaddress = new ListenAddress(args[0], null);
+		} else if (args.length >= 2) {
+			listenaddress = new ListenAddress(args[0], args[1]);
+		} else {
+			listenaddress = new ListenAddress();
+		}
+		init();
+	}
 
-  private void init() {
-  }
+	private void init() {
+	}
 
-  public void configure() {
-    System.out.println("Version:" + getServer());
-    System.out.println("Listen On:" + listenaddress.toString());
-    from(
-        "restlet:http://" + listenaddress.toString()
-            + "/traffic/byTwitter/{screenName}?restletMethods=get").bean(this,
-        "byTwitter");
-    from(
-        "restlet:http://" + listenaddress.toString()
-            + "/traffic/top10?restletMethods=get").bean(this,
-        "top10");
-  }
+	public void configure() {
+		System.out.println("Version:" + getServer());
+		System.out.println("Listen On:" + listenaddress.toString());
+		from(
+				"restlet:http://" + listenaddress.toString()
+						+ "/traffic/byTwitter/{screenName}?restletMethods=get")
+				.bean(this, "byTwitter");
+		from(
+				"restlet:http://" + listenaddress.toString()
+						+ "/traffic/top10?restletMethods=get").bean(this,
+				"top10");
+	}
 
-  private String _version = null;
+	private String _version = null;
 
-  private String getServer() {
-    if (_version != null) {
-      return _version;
-    }
-    synchronized (this) {
-      if (_version != null) {
-        return _version;
-      }
-      String version = "Informer(development)";
-      final InputStream is = Router.class.getClassLoader().getResourceAsStream(
-          "META-INF/maven/com.adviser.informer/informer/pom.xml");
-      if (is != null) {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        Document doc;
-        try {
-          DocumentBuilder db = dbf.newDocumentBuilder();
-          doc = db.parse(is);
-          version = "Informer("
-              + doc.getElementsByTagName("version").item(0).getTextContent()
-              + ")";
-        } catch (Exception e) {
-          // System.out.println("IS:"+e.getMessage());
-        }
-      }
-      _version = version;
-    }
-    return _version;
-  }
+	private String getServer() {
+		if (_version != null) {
+			return _version;
+		}
+		synchronized (this) {
+			if (_version != null) {
+				return _version;
+			}
+			String version = "Informer(development)";
+			final InputStream is = Router.class
+					.getClassLoader()
+					.getResourceAsStream(
+							"META-INF/maven/com.adviser.informer/informer/pom.xml");
+			if (is != null) {
+				DocumentBuilderFactory dbf = DocumentBuilderFactory
+						.newInstance();
+				Document doc;
+				try {
+					DocumentBuilder db = dbf.newDocumentBuilder();
+					doc = db.parse(is);
+					version = "Informer("
+							+ doc.getElementsByTagName("version").item(0)
+									.getTextContent() + ")";
+				} catch (Exception e) {
+					// System.out.println("IS:"+e.getMessage());
+				}
+			}
+			_version = version;
+		}
+		return _version;
+	}
 
-  public void byTwitter(Exchange exchange) {
-    final Message _in = exchange.getIn();
-    final String screenName = _in.getHeader("screenName", String.class);
-    final Message _out = exchange.getOut();
-    Streamie streamie = streamies.findByTwitter(screenName);
+	public void byTwitter(Exchange exchange) {
+		final Message _in = exchange.getIn();
+		final String screenName = _in.getHeader("screenName", String.class);
+		final Message _out = exchange.getOut();
+		Streamie streamie = streamies.findByTwitter(screenName);
 
-    StringWriter str = new StringWriter();
-    try {
-      (new ObjectMapper()).writeValue(str, streamie);
-    } catch (JsonGenerationException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (JsonMappingException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    _out.setBody(str.toString());
-  }
-  
-  public void top10(Exchange exchange) {
-    final Iterator<Streamie> top10 = streamies.top(10).iterator();
-    final List<String> screenNames = new LinkedList<String>();
-    while (top10.hasNext()) {
-      final Streamie s = top10.next();
-      screenNames.add(s.getTwitter().getScreen_name());
-    }  
-    final Message _out = exchange.getOut();
-    _out.setBody(screenNames.toString());
-  }
+		StringWriter str = new StringWriter();
+		try {
+			(new ObjectMapper()).writeValue(str, streamie);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		_out.setBody(str.toString());
+	}
+
+	public void top10(Exchange exchange) {
+		final Message _out = exchange.getOut();
+		StringWriter str = new StringWriter();
+		try {
+			(new ObjectMapper()).writeValue(str, streamies.top(10));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		_out.setBody(str.toString());
+	}
 }
--- informer/src/main/java/com/adviser/informer/model/Streamies.java
+++ informer/src/main/java/com/adviser/informer/model/Streamies.java
@@ -1,5 +1,6 @@
 package com.adviser.informer.model;
 
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
@@ -7,12 +8,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Observable;
+import java.util.PriorityQueue;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
+
+import lombok.Data;
 
 import org.apache.commons.collections.list.SynchronizedList;
 import org.ektorp.changes.ChangesCommand;
@@ -23,245 +27,267 @@ import org.ektorp.impl.StdCouchDbConnector;
 import com.adviser.informer.model.streamie.Client;
 import com.adviser.informer.model.streamie.History;
 import com.adviser.informer.model.streamie.Streamie;
+import com.adviser.informer.model.streamie.TopItem;
+import com.adviser.informer.model.streamie.Total;
 import com.adviser.informer.model.traffic.IpTuple;
 import com.adviser.informer.model.traffic.Traffic;
 
 public class Streamies extends Observable implements Runnable {
 
-  public static final int INITIALIZE = 1;
+	public static final int INITIALIZE = 1;
 
-  private class ByTraffic {
-    private static final long serialVersionUID = 4981832330424047439L;
+	private class ByTraffic implements Runnable, Serializable {
+		private static final long serialVersionUID = 4981832330424047439L;
 
-    private PriorityBlockingQueue<Streamie> traffic = new PriorityBlockingQueue<Streamie>(1000, new Comparator<Streamie>(){
+		private final Map<String, Streamie> ids;
+		public ByTraffic(Map<String, Streamie> _ids) {
+			ids = _ids;
+		}
+		private PriorityQueue<Streamie> traffic = null;
 
-      public int compare(Streamie arg0, Streamie arg1) {
-        final long ret = getTotal(arg1)-getTotal(arg0);
-        if (ret > 0) {
-          return 1;
-        } else if (ret < 0) {
-          return -1;
-        }
-        return 0;
-      }
-      public long getTotal(Streamie streamie) {
-        return streamie.getInTotal() + streamie.getOutTotal();
-      }
-    });
-  
-    public void update(Long preTraffic, Long newTraffic, Streamie streamie) {
-      if (preTraffic.compareTo(newTraffic) == 0) {
-        return;
-      }
-      synchronized(streamie) {
-        traffic.remove(streamie);
-        traffic.add(streamie);
-      }
-    }
+		public List<Streamie> top(int count) {
+			final List<Streamie> ret = new LinkedList<Streamie>();
+			if (traffic == null) {
+				return ret;
+			}
+			final Iterator<Streamie> i = traffic.iterator();
+			for (int j = 0; j < count && i.hasNext(); ++j) {
+				ret.add(i.next());
+			}
+			return ret;
+		}
 
-    public List<Streamie> top(int count) {
-      final List<Streamie> ret = new LinkedList<Streamie>();
-      final Iterator<Streamie> i = traffic.iterator();
-      for(int j =0; j < 10 && i.hasNext(); ++j) {
-        ret.add(i.next());
-      }
-      return ret;
-    }
-  }
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(60 * 1000);
+					System.out.println("Start TOP10");
+					final PriorityQueue<Streamie> tmp = new PriorityQueue<Streamie>(ids.size(),
+							new Comparator<Streamie>() {
 
-  private Map<String, Streamie> byId = null;
-  private Map<String, Streamie> byTwitter = null;
-  private Map<String, Streamie> byIp = null;
-  private Map<String, Streamie> byMac = null;
-  private ByTraffic byTraffic = null;
-  private History totalTraffic = null;
+								public int compare(Streamie arg0, Streamie arg1) {
+									final long ret = getTotal(arg1)
+											- getTotal(arg0);
+									if (ret > 0) {
+										return 1;
+									} else if (ret < 0) {
+										return -1;
+									}
+									return 0;
+								}
 
-  private Streamies() {
-    totalTraffic = new History();
-    byId = new ConcurrentHashMap<String, Streamie>();
-    byTwitter = new ConcurrentHashMap<String, Streamie>();
-    byIp = new ConcurrentHashMap<String, Streamie>();
-    byMac = new ConcurrentHashMap<String, Streamie>();
-    byTraffic = new ByTraffic();
-  }
+								public long getTotal(Streamie streamie) {
+									return streamie.getInTotal()
+											+ streamie.getOutTotal();
+								}
+							});
+					Iterator<Streamie> i = ids.values().iterator();
+					while (i.hasNext()) {
+						final Streamie streamie = i.next();
+						tmp.add(streamie);
+					}
+					System.out.println("Done TOP10");
+					traffic = tmp;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-  public Streamie findById(String id) {
-    return byId.get(id);
-  }
+			}
+		}
+	}
 
-  public Streamie findByTwitter(String screename) {
-    return byTwitter.get(screename);
-  }
+	private final Map<String, Streamie> byId;
+	private final Map<String, Streamie> byTwitter;
+	private final Map<String, Streamie> byIp;
+	private final Map<String, Streamie> byMac;
+	private final ByTraffic byTraffic;
+	private final History totalTraffic;
 
-  private void remove(String id) {
-    if (id == null) {
-      return;
-    }
-    final Streamie str = byId.get(id);
-    if (str == null) {
-      return;
-    }
-    byId.remove(str.getId());
-    byTwitter.remove(str.getTwitter().getScreen_name());
-    final Iterator<Client> clients = str.getClients().iterator();
-    while (clients.hasNext()) {
-      final Client client = clients.next();
-      byIp.remove(client.getIpv4());
-      byMac.remove(client.getHwaddr());
-    }
-  }
+	private Streamies() {
+		totalTraffic = new History();
+		byId = new ConcurrentHashMap<String, Streamie>();
+		byTwitter = new ConcurrentHashMap<String, Streamie>();
+		byIp = new ConcurrentHashMap<String, Streamie>();
+		byMac = new ConcurrentHashMap<String, Streamie>();
+		byTraffic = new ByTraffic(byId);
+	}
 
-  private void add(Streamie str) {
-    remove(str.getId());
-    byId.put(str.getId(), str);
-    byTwitter.put(str.getTwitter().getScreen_name(), str);
-    final Iterator<Client> clients = str.getClients().iterator();
-    while (clients.hasNext()) {
-      final Client client = clients.next();
-      byIp.put(client.getIpv4(), str);
-      byMac.put(client.getHwaddr(), str);
-    }
-  }
+	public Streamie findById(String id) {
+		return byId.get(id);
+	}
 
-  private static Streamies my = null;
+	public Streamie findByTwitter(String screename) {
+		return byTwitter.get(screename);
+	}
 
-  public static Streamies init() {
-    if (my == null) {
-      my = new Streamies();
-      new Thread(my).start();
-    }
-    return my;
-  }
+	private void remove(String id) {
+		if (id == null) {
+			return;
+		}
+		final Streamie str = byId.get(id);
+		if (str == null) {
+			return;
+		}
+		byId.remove(str.getId());
+		byTwitter.remove(str.getTwitter().getScreen_name());
+		final Iterator<Client> clients = str.getClients().iterator();
+		while (clients.hasNext()) {
+			final Client client = clients.next();
+			byIp.remove(client.getIpv4());
+			byMac.remove(client.getHwaddr());
+		}
+	}
 
-  private class Fetchers implements Runnable {
+	private void add(Streamie str) {
+		remove(str.getId());
+		byId.put(str.getId(), str);
+		byTwitter.put(str.getTwitter().getScreen_name(), str);
+		final Iterator<Client> clients = str.getClients().iterator();
+		while (clients.hasNext()) {
+			final Client client = clients.next();
+			byIp.put(client.getIpv4(), str);
+			byMac.put(client.getHwaddr(), str);
+		}
+	}
 
-    private BlockingQueue<DocumentChange> q;
-    private Completed c;
+	private static Streamies my = null;
 
-    public Fetchers(BlockingQueue<DocumentChange> _q, Completed _c) {
-      q = _q;
-      c = _c;
-    }
+	public static Streamies init() {
+		if (my == null) {
+			my = new Streamies();
+			new Thread(my).start();
+		}
+		return my;
+	}
 
-    public void run() {
-      final StdCouchDbConnector db = new StdCouchDbConnector("streamie",
-          CouchDB.connection());
-      try {
-        while (true) {
-          final DocumentChange dc = q.take();
-          if (dc.isDeleted()) {
-            remove(dc.getId());
-          } else {
-            final Streamie streamie = db.get(Streamie.class, dc.getId());
-            add(streamie);
-          }
-          System.out.println("Streamie:" + dc.getId());
-          c.done(dc.getSequence());
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
+	private class Fetchers implements Runnable {
 
-    }
-  }
+		private BlockingQueue<DocumentChange> q;
+		private Completed c;
 
-  public abstract class Completed {
+		public Fetchers(BlockingQueue<DocumentChange> _q, Completed _c) {
+			q = _q;
+			c = _c;
+		}
 
-    private long last = 0;
+		public void run() {
+			final StdCouchDbConnector db = new StdCouchDbConnector("streamie",
+					CouchDB.connection());
+			try {
+				while (true) {
+					final DocumentChange dc = q.take();
+					if (dc.isDeleted()) {
+						remove(dc.getId());
+					} else {
+						final Streamie streamie = db.get(Streamie.class,
+								dc.getId());
+						add(streamie);
+					}
+					System.out.println("Streamie:" + dc.getId());
+					c.done(dc.getSequence());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-    public Completed(long _last) {
-      last = _last;
-    }
+		}
+	}
 
-    public void done(long seq) {
-      if (seq == last) {
-        completed(seq);
-      }
-    }
+	public abstract class Completed {
 
-    public abstract void completed(long last);
+		private long last = 0;
 
-  }
+		public Completed(long _last) {
+			last = _last;
+		}
 
-  public void add(Traffic traffic) {
-    final Iterator<IpTuple> tuples = traffic.getTuples().iterator();
-    while (tuples.hasNext()) {
-      final IpTuple tuple = tuples.next();
-      long inAmount = 0;
-      long outAmount = 0;
-      String matchIp = tuple.getDstIP();
-      Streamie streamie = byIp.get(matchIp);
-      if (streamie == null) {
-        streamie = byTwitter.get(matchIp);
-        if (streamie == null) {
-          matchIp = tuple.getSrcIP();
-          streamie = byIp.get(matchIp);
-          if (streamie == null) {
-            streamie = byTwitter.get(matchIp);
-            if (streamie == null) {
-              continue;
-            } else {
-              outAmount = tuple.getOctets();
-            }
-          } else {
-            outAmount = tuple.getOctets();
-          }
-        } else {
-          inAmount = tuple.getOctets();
-        }
-      } else {
-        inAmount = tuple.getOctets();
-      }
-      final long timeStamp = traffic.getCreatedAt().getTime() / 1000;
-      totalTraffic.add(timeStamp, inAmount, outAmount);
-      final Long preTraffic = new Long(streamie.getInTotal()
-          + streamie.getOutTotal());
-      streamie.add(matchIp, timeStamp, inAmount, outAmount);
-      final Long postTraffic = new Long(streamie.getInTotal()
-          + streamie.getOutTotal());
-      byTraffic.update(preTraffic, postTraffic, streamie);
-    }
-  }
+		public void done(long seq) {
+			if (seq == last) {
+				completed(seq);
+			}
+		}
 
-  public List<Streamie> top(int count) {
-    return byTraffic.top(count);
-  }
+		public abstract void completed(long last);
 
-  public void run() {
+	}
 
-    final BlockingQueue<DocumentChange> q = new LinkedBlockingQueue<DocumentChange>();
-    final ChangesCommand cmd = new ChangesCommand.Builder().since(0).build();
-    final StdCouchDbConnector db = new StdCouchDbConnector("streamie",
-        CouchDB.connection());
-    final List<DocumentChange> feed = db.changes(cmd);
+	public void add(Traffic traffic) {
+		final Iterator<IpTuple> tuples = traffic.getTuples().iterator();
+		while (tuples.hasNext()) {
+			final IpTuple tuple = tuples.next();
+			long inAmount = 0;
+			long outAmount = 0;
+			String matchIp = tuple.getDstIP();
+			Streamie streamie = byIp.get(matchIp);
+			if (streamie == null) {
+				streamie = byTwitter.get(matchIp);
+				if (streamie == null) {
+					matchIp = tuple.getSrcIP();
+					streamie = byIp.get(matchIp);
+					if (streamie == null) {
+						streamie = byTwitter.get(matchIp);
+						if (streamie == null) {
+							continue;
+						} else {
+							outAmount = tuple.getOctets();
+						}
+					} else {
+						outAmount = tuple.getOctets();
+					}
+				} else {
+					inAmount = tuple.getOctets();
+				}
+			} else {
+				inAmount = tuple.getOctets();
+			}
+			final long timeStamp = traffic.getCreatedAt().getTime() / 1000;
+			totalTraffic.add(timeStamp, inAmount, outAmount);
+			streamie.add(matchIp, timeStamp, inAmount, outAmount);
+		}
+	}
 
-    q.addAll(feed);
-    System.out.println("Initial Read until len:" + feed.size() + ":"
-        + feed.get(feed.size() - 1).getSequence());
+	public List<Streamie> top(int count) {
+		return byTraffic.top(count);
+	}
 
-    final Streamies self = this;
-    final Completed c = new Completed(feed.get(feed.size() - 1).getSequence()) {
+	public void run() {
 
-      @Override
-      public void completed(long last) {
-        System.out.println("Initial Read completed until:" + last);
-        final ChangesCommand cmd = new ChangesCommand.Builder().since(last)
-            .build();
-        final ChangesFeed feed = db.changesFeed(cmd);
-        self.setChanged();
-        self.notifyObservers(Streamies.INITIALIZE);
-        while (feed.isAlive()) {
-          try {
-            final DocumentChange change = feed.next();
-            q.add(change);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-        }
+		final BlockingQueue<DocumentChange> q = new LinkedBlockingQueue<DocumentChange>();
+		final ChangesCommand cmd = new ChangesCommand.Builder().since(0)
+				.build();
+		final StdCouchDbConnector db = new StdCouchDbConnector("streamie",
+				CouchDB.connection());
+		final List<DocumentChange> feed = db.changes(cmd);
 
-      }
-    };
-    new Thread(new Fetchers(q, c)).start();
-    new Thread(new Fetchers(q, c)).start();
-  }
+		q.addAll(feed);
+		System.out.println("Initial Read until len:" + feed.size() + ":"
+				+ feed.get(feed.size() - 1).getSequence());
+
+		final Streamies self = this;
+		final Completed c = new Completed(feed.get(feed.size() - 1)
+				.getSequence()) {
+
+			@Override
+			public void completed(long last) {
+				System.out.println("Initial Read completed until:" + last);
+				final ChangesCommand cmd = new ChangesCommand.Builder().since(
+						last).build();
+				final ChangesFeed feed = db.changesFeed(cmd);
+				self.setChanged();
+				self.notifyObservers(Streamies.INITIALIZE);
+				while (feed.isAlive()) {
+					try {
+						final DocumentChange change = feed.next();
+						q.add(change);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+		};
+		new Thread(new Fetchers(q, c)).start();
+		new Thread(new Fetchers(q, c)).start();
+	}
 }
